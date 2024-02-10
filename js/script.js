ccea5a3db6cd9a0717a973dd589f57495394b200
Hasib Al Muzdadid <muzdadid@gmail.com>
2024-02-10 08:58:34 +0600

    Horizontal navigation bar to vertical navigation bar

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+    });
+
+    $(window).on('scroll load',function(){
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+    });
+
+});
+
+
+/* Copyright 2022-2024 Hasib Al Muzdadid | All Rights Reserved */
